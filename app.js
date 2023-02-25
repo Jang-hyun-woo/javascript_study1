a5ba22cbb916a7873556f29ec085871f3bd622a0
hyunwoo <zzanghw91@naver.com>
2023-02-25 17:17:25 +0900

    노트북

---
+++ app.js
@@ -0,0 +1,12 @@
+let age = 32;
+let userName = 'HyunWoo';
+let hobbies = ['Sports', 'Cooking', 'reading', 1];
+let job = {
+    title: 'Developer',
+    place: 'New York',
+    salary: 50000
+};
+
+let adultYears
+
+adultYears = age - 18;
