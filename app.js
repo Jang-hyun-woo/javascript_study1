552a695dbc29f831a62dce2b44106707bb3142be
HyunWoo <zzanghw91@naver.com>
2023-02-28 23:23:02 +0900

    기초 완료

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 let age = 32;
-let userName = 'HyunWoo';
-let hobbies = ['Sports', 'Cooking', 'reading', 1];
+let userName = "HyunWoo";
+let hobbies = ["Sports", "Cooking", "reading", 1];
 let job = {
-    title: 'Developer',
-    place: 'New York',
-    salary: 50000
+  title: "Developer",
+  place: "New York",
+  salary: 50000,
 };
 
-let adultYears
+let totalAdultYears;
 
-adultYears = age - 18;
+function calculateAdultYear(userAge) {
+  let result;
+  result = userAge - 18;
+  return result;
+}
+
+totalAdultYears = calculateAdultYear(32);
+alert(totalAdultYears);
+
+age = 45;
+totalAdultYears = calculateAdultYear(age);
+console.log(totalAdultYears);
+
+console.log(totalAdultYears);
+
+let person = {
+  //속성
+  name: "hyunwoo",
+  //메소드
+  greet() {
+    console.log("Hello!");
+  },
+};
+
+person.greet();
---
+++ operations.js
@@ -0,0 +1,31 @@
+//Math operations
+
+console.log(10 + 4);
+console.log(10 - 4);
+console.log(10 * 4);
+console.log(10 / 4);
+console.log(10 % 4);
+
+console.log(10 + 3 - 5.1 * 10);
+
+let result = 10 + 3 - 5.1 * 10;
+result = result + 1;
+result++;
+result--;
+
+/*
+result += 5;
+result -= 5;
+result *= 5;
+result /= 5;
+*/
+
+console.log("HyunWoo" + "Jang");
+
+let userName = "Jang";
+console.log(userName.length);
+console.log(userName.toUpperCase());
+
+let hobbies = ["Sports", "Cooking"];
+console.log(hobbies.length);
+console.log(hobbies.sort());
